eb44f150c4617a704340ea67daea17232bd104d2
farukalways <alishopon367@gmail.com>
2025-09-28 23:42:56 +0600

    routing

---
+++ 01-installation/app/dashboard/setting/page.js
@@ -0,0 +1,14 @@
+const Setting = () => {
+  return (
+    <div>
+      <h2>setting</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat debitis
+        impedit eum perspiciatis iure, ipsa inventore quidem esse voluptate enim
+        labore in ex molestias id suscipit voluptatum tempora sequi architecto.
+      </p>
+    </div>
+  );
+};
+
+export default Setting;
