0e93765924f9eb01ab09959850dffa88b725c3b2
farukalways <alishopon367@gmail.com>
2025-09-29 07:48:09 +0600

    learn route, page, layout, template

---
+++ 03-routes-page-layout/app/dashboard/setting/page.js
@@ -0,0 +1,14 @@
+const Setting = () => {
+  return (
+    <div>
+      <h2>Analytics</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel iure
+        beatae rem, pariatur ut illo numquam obcaecati excepturi, atque eum
+        quam. Ipsa facilis accusamus quidem excepturi nesciunt, autem quos sed!
+      </p>
+    </div>
+  );
+};
+
+export default Setting;
---
+++ 03-routes-page-layout/app/page.js
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>home</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta
+        doloremque error veniam iure illo atque asperiores nemo explicabo
+        facilis nulla necessitatibus, a itaque doloribus. Consequatur sunt
+        distinctio esse ut perferendis.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
